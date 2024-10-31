6cc8ae28d6a097391194defbd7f9718356035962
rod1gers <rodgersochieng143@gmail.com>
2024-10-31 10:04:02 +0300

    map added

--- src/pages/app/App.tsx
+++ src/pages/app/App.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import FlightForm from '../../components/FlightForm';
 import FlightList from '../../components/FlightList';
+import { Link } from "react-router-dom";
 
 export interface Flight {
     departure: string;
@@ -19,9 +20,14 @@ function App() {
 
     return (
         <div className="max-w-2xl mx-auto p-5">
+
             <h1 className="text-3xl font-bold text-center mb-5">Airline Route Planner</h1>
             <FlightForm addFlight={addFlight} />
             <FlightList flights={flights} />
+
+            <div className='bg-gradient-to-tr from-black to-gray-800 w-fit px-2 py-1 rounded-lg'>
+                <Link to="/map" className='text-white cursor-pointer'>Go to Map Page</Link>
+            </div>
         </div>
     );
 }
